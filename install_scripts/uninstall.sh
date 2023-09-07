1d26b4d2131f205abc3c220381d84af50dd98d60
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-09-07 11:08:24 -0300

    update uninstall.sh

--- install_scripts/uninstall.sh
+++ install_scripts/uninstall.sh
@@ -3,12 +3,16 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
+set -e
+
 print_colored_message() {
     local color="$1"
     local message="$2"
     
     printf "%s\n" "${color}${message}${NORMAL}"
 }
+
+print_colored_message "$GREEN" "Uninstalling Splight agent"
 
 docker stop splight-agent --time 600
 docker rm splight-agent
