975c0d0a32f0708951157bb74c4d449422218e14
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-10-03 11:05:27 -0300

    changed func name in uninstall

--- install_scripts/uninstall.sh
+++ install_scripts/uninstall.sh
@@ -1,14 +1,14 @@
 set -e
 
-print_colored_message() {
+print_message() {
     local message="$1"
     
     printf "%s\n" "${message}"
 }
 
-print_colored_message "Uninstalling Splight agent"
+print_message "Uninstalling Splight agent"
 
 docker stop splight-agent --time 600
 docker rm splight-agent
 
-print_colored_message "Splight agent uninstalled successfully."
+print_message "Splight agent uninstalled successfully."
