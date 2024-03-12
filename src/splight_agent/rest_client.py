c940fb36915f93fe86123e25cf2e1abdcf8009c7
Lautaro Lecumberry <lautarolecumberry@users.noreply.github.com>
2024-03-12 13:16:38 -0300

    fix code style

--- src/splight_agent/rest_client.py
+++ src/splight_agent/rest_client.py
@@ -39,8 +39,10 @@ class RestClient:
         response.raise_for_status()
         return response
 
-    def get(self, path: str, params: dict=None) -> requests.Response:
-        response = requests.get(self._base_url / path, headers=self.headers, params=params)
+    def get(self, path: str, params: dict = None) -> requests.Response:
+        response = requests.get(
+            self._base_url / path, headers=self.headers, params=params
+        )
         response.raise_for_status()
         return response
 
